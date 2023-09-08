1a081359d0cc6eb3369556341867c13704b207b2
Siverena <vpupkin@company.com>
2023-09-08 15:26:02 +0300

    forms

--- components/fs-achievements/FsAchievements.vue
+++ components/fs-achievements/FsAchievements.vue
@@ -8,7 +8,7 @@
         <div class="fs-achievements__items">
           <div class="fs-achievements__item">
             <FsHat />
-            <p class="fs-achievements__item-title">Лучший класс школы</p>
+            <p class="fs-achievements__item-title fs-h2">Лучший класс школы</p>
             <p class="fs-achievements__item-text">
               Поздравляем наших учеников с заслуженной победой в конкурсе
               талантов. Приглашаем на церемонию награждения!
@@ -16,7 +16,7 @@
           </div>
           <div class="fs-achievements__item">
             <FsExcellent />
-            <p class="fs-achievements__item-title">8 отличников</p>
+            <p class="fs-achievements__item-title fs-h2">8 отличников</p>
             <p class="fs-achievements__item-text">
               Поздравляем наших учеников с заслуженной победой в конкурсе
               талантов. Приглашаем на церемонию награждения!
@@ -24,7 +24,9 @@
           </div>
           <div class="fs-achievements__item">
             <FsOlympic />
-            <p class="fs-achievements__item-title">1 место на олимпиаде </p>
+            <p class="fs-achievements__item-title fs-h2">
+              1 место на олимпиаде
+            </p>
             <p class="fs-achievements__item-text">
               Поздравляем наших учеников с заслуженной победой в конкурсе
               талантов. Приглашаем на церемонию награждения!
