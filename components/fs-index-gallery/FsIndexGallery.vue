97c4f8f004f7dab5a9bcf1e7d70890e30fa38900
Marin-A-S <106108224+Marin-A-S@users.noreply.github.com>
2023-09-22 23:19:22 +0300

    NutritionQuestions form added

--- components/fs-index-gallery/FsIndexGallery.vue
+++ components/fs-index-gallery/FsIndexGallery.vue
@@ -1,64 +1,46 @@
 <template>
-  <section class="fs-index-gallery">
-    <div class="container">
-      <div class="fs-index-gallery__content">
-        <FsSectionTitles class="fs-index-gallery__title">
-          <template v-slot:h1>Галерея</template>
-        </FsSectionTitles>
-        <div class="fs-index-gallery__loader" v-if="loading">
-          <div class="fs-index-gallery__loader-col">
-            <div></div>
-            <div></div>
-          </div>
-          <div class="fs-index-gallery__loader-right">
-            <div class="fs-index-gallery__loader-cards">
-              <div
-                class="fs-index-gallery__loader-card"
-                v-for="key in 3"
-                :key="key"
-              >
-                <div class="fs-index-gallery__loader-title"></div>
-              </div>
-            </div>
-          </div>
+  <section class="fs-index-gallery" v-if="!loading">
+    <div class="fs-index-gallery__content">
+      <FsSectionTitles class="fs-index-gallery__title">
+        <template v-slot:h1>Галерея</template>
+      </FsSectionTitles>
+      <div class="fs-index-gallery__layout">
+        <div class="fs-index-gallery__info">
+          <p class="fs-h2">Жизнь нашей школы в фотографиях</p>
+
+          <NuxtLink
+            class="fs-link fs-link--green fs-index-gallery__link"
+            to="/gallery"
+            >Посмотреть все альбомы</NuxtLink
+          >
         </div>
-        <div class="fs-index-gallery__layout" v-if="!loading">
-          <div class="fs-index-gallery__info">
-            <p class="fs-h2">Жизнь нашей школы в фотографиях</p>
-            <NuxtLink
-              class="fs-link fs-link--green fs-index-gallery__link"
-              to="/gallery"
-              >Посмотреть все альбомы</NuxtLink
+        <div class="fs-index-gallery__reasons-cont">
+          <div class="fs-index-gallery__reasons">
+            <div
+              class="fs-index-gallery__reason"
+              v-for="(reason, key) in reasons"
+              :key="key"
             >
-          </div>
-          <div class="fs-index-gallery__reasons-cont">
-            <div class="fs-index-gallery__reasons">
-              <div
-                class="fs-index-gallery__reason"
-                v-for="(reason, key) in reasons"
-                :key="key"
-              >
-                <nuxt-link :to="`/gallery/${reason.nameEng}`">
-                  <img :src="reason.mainImg" alt="" />
-                  <div class="fs-index-gallery__reason-text">
-                    {{ reason.name }}
-                  </div>
-                </nuxt-link>
-              </div>
+              <nuxt-link :to="`/gallery/${reason.nameEng}`">
+                <img :src="getStaticImageUrl(reason.mainImg)" alt="" />
+                <div class="fs-index-gallery__reason-text">
+                  {{ reason.name }}
+                </div>
+              </nuxt-link>
             </div>
-            <div class="fs-index-gallery__nav">
-              <FsBtnSlidesPrev @click="prevReason" />
-              <FsBtnSlidesNext @click="nextReason" />
-              <div class="fs-index-gallery__count">
-                {{ activeReason + 1 }} / {{ reasonsCount }}
-              </div>
-            </div>
-            <NuxtLink
-              class="fs-link fs-link--green fs-index-gallery__link fs-index-gallery__link--mobile"
-              to="/gallery"
-              >Посмотреть все фото</NuxtLink
-            >
           </div>
+          <div class="fs-index-gallery__nav">
+            <FsBtnSlidesPrev @click="prevReason" />
+            <FsBtnSlidesNext @click="nextReason" />
+            <div class="fs-index-gallery__count">
+              {{ activeReason + 1 }} / {{ reasonsCount }}
+            </div>
+          </div>
+          <NuxtLink
+            class="fs-link fs-link--green fs-index-gallery__link fs-index-gallery__link--mobile"
+            to="/gallery"
+            >Посмотреть все фото</NuxtLink
+          >
         </div>
       </div>
     </div>
--- stores/navLinksStore.js
+++ stores/navLinksStore.js
@@ -3,32 +3,20 @@ export const useNavLinksStore = defineStore('NavLinksStore', {
   state: () => {
     return {
       navLinks: [
-        // {
-        //   name: 'О нас ',
-        //   link: '/',
-        //   visibleInMenu: true,
-        //   visibleInSvedenMenu: false,
-        //   child: [
-        //     {
-        //       name: 'Новости',
-        //       link: '/news',
-        //       visibleInMenu: true,
-        //       visibleInSvedenMenu: false,
-        //     },
-        //     {
-        //       name: 'Галерея',
-        //       link: '/gallery',
-        //       visibleInMenu: true,
-        //       visibleInSvedenMenu: false,
-        //     },
-        //     {
-        //       name: 'Контакты',
-        //       link: '/contacts',
-        //       visibleInMenu: true,
-        //       visibleInSvedenMenu: false,
-        //     },
-        //   ],
-        // },
+        {
+          name: 'О нас ',
+          link: '/',
+          visibleInMenu: true,
+          visibleInSvedenMenu: false,
+          child: [
+            {
+              name: 'Сведения об образовательной организации',
+              link: '/sveden',
+              visibleInMenu: true,
+              visibleInSvedenMenu: true,
+            },
+          ],
+        },
         {
           name: 'Новости',
           link: '/news',
@@ -46,92 +34,6 @@ export const useNavLinksStore = defineStore('NavLinksStore', {
           link: '/schedule',
           visibleInMenu: true,
           visibleInSvedenMenu: false,
-        },
-        {
-          name: 'Сведения об образовательной организации',
-          link: '/sveden',
-          visibleInMenu: true,
-          visibleInSvedenMenu: false,
-          child: [
-            {
-              name: 'Основные сведения',
-              link: '/sveden/common',
-              visibleInMenu: false,
-              visibleInSvedenMenu: true,
-            },
-            {
-              name: 'Структура и органы управления образовательной организации',
-              link: '/sveden/struct',
-              visibleInMenu: false,
-              visibleInSvedenMenu: true,
-            },
-            {
-              name: 'Документы',
-              link: '/sveden/document',
-              visibleInMenu: false,
-              visibleInSvedenMenu: true,
-            },
-            {
-              name: 'Образование',
-              link: '/sveden/education',
-              visibleInMenu: false,
-              visibleInSvedenMenu: true,
-            },
-            {
-              name: 'Образовательные стандарты',
-              link: '/sveden/eduStandarts',
-              visibleInMenu: false,
-              visibleInSvedenMenu: true,
-            },
-            {
-              name: 'Руководство. Педагогический состав',
-              link: '/sveden/employees',
-              visibleInMenu: false,
-              visibleInSvedenMenu: true,
-            },
-            {
-              name: 'Материально-техническое обеспечение и оснащенность образовательного процесса',
-              link: '/sveden/objects',
-              visibleInMenu: false,
-              visibleInSvedenMenu: true,
-            },
-            {
-              name: 'Стипендии и меры поддержки обучающихся',
-              link: '/sveden/grants',
-              visibleInMenu: false,
-              visibleInSvedenMenu: true,
-            },
-            {
-              name: 'Платные образовательные услуги',
-              link: '/sveden/paid_edu',
-              visibleInMenu: false,
-              visibleInSvedenMenu: true,
-            },
-            {
-              name: 'Финансово-хозяйственная деятельность',
-              link: '/sveden/budget',
-              visibleInMenu: false,
-              visibleInSvedenMenu: true,
-            },
-            {
-              name: 'Вакантные места для приема (перевода)',
-              link: '/sveden/vacant',
-              visibleInMenu: false,
-              visibleInSvedenMenu: true,
-            },
-            {
-              name: 'Международное сотрудничество',
-              link: '/sveden/cooperation',
-              visibleInMenu: false,
-              visibleInSvedenMenu: false,
-            },
-            {
-              name: 'Доступная среда',
-              link: '/sveden/dsreda',
-              visibleInMenu: false,
-              visibleInSvedenMenu: false,
-            },
-          ],
         },
         {
           name: 'Контакты',
@@ -233,13 +135,27 @@ export const useNavLinksStore = defineStore('NavLinksStore', {
           name: 'Международное сотрудничество',
           link: '/sveden/cooperation',
           visibleInMenu: false,
-          visibleInSvedenMenu: true,
+          visibleInSvedenMenu: false,
         },
         {
           name: 'Доступная среда',
           link: '/sveden/dsreda',
           visibleInMenu: false,
+          visibleInSvedenMenu: false,
+        },
+
+        {
+          name: 'Школьное питание',
+          link: '/ezhednevnoe-menyu',
+          visibleInMenu: false,
           visibleInSvedenMenu: true,
+        },
+
+        {
+          name: 'Формы обратной связи для родителей обучающихся и ответы на вопросы родителей по питанию',
+          link: '/formy-obratnoj-svyazi-dlya-roditelej-obuchayushhihsya-i-otvety-na-voprosy-roditelej-po-pitaniyu',
+          visibleInMenu: false,
+          visibleInSvedenMenu: false,
         },
       ],
     };
