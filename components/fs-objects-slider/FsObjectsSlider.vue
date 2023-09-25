0c7d6f53706378094287722425f38e2f8e2a6192
DmitriyYar <1120597@mail.ru>
2023-09-25 20:41:09 +0400

    added objects

---
+++ components/fs-objects-slider/FsObjectsSlider.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="fs-objects-slider">
+    <div class="fs-objects-slider__images">
+      <div v-for="(image, index) in arrCabinet.images" :key="index">
+        <div class="fs-objects-slider__images-item" v-if="index === currentSlide">
+          <img :src="getStaticImageUrl(image)" alt="{{image}}" />
+        </div>
+      </div>
+    </div>
+    <div class="fs-objects-slider__nav" :class="{ 'fs-objects-slider__nav--rev': inCabinet % 2 }">
+      <div class="fs-objects-slider__count">{{ currentSlide + 1 }} / {{ slidesCount }}</div>
+      <FsBtnSlidesPrev @click="prevSlide" :class="{ 'fs-objects-slider__btn--rev': inCabinet % 2 }" />
+      <FsBtnSlidesNext @click="nextSlide" :class="{ 'fs-objects-slider__btn--rev': inCabinet % 2 }" />
+    </div>
+  </div>
+</template>
+
+<script>
+import imageUrl from "@/utils/mixins/image-url.js";
+export default {
+  data() {
+    return {
+      currentSlide: 0,
+    };
+  },
+  props: ["arrCabinet", "inCabinet"],
+  mixins: [imageUrl],
+
+  computed: {
+    slidesCount() {
+      return this.arrCabinet.images.length;
+    },
+  },
+  methods: {
+    nextSlide() {
+      if (this.currentSlide === this.arrCabinet.images.length - 1) {
+        this.currentSlide = 0;
+      } else {
+        this.currentSlide += 1;
+      }
+    },
+    prevSlide() {
+      this.currentSlide -= 1;
+      if (this.currentSlide < 0) {
+        this.currentSlide = this.arrCabinet.images.length - 1;
+      }
+    },
+  },
+};
+</script>
---
+++ stores/data/d-objects.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    title: "Кабинет информатики",
+    text: "Объект оснащен 6 персональными компьютерами 1 из которых с подключением к сети Интернет. Кабинет предназначен для изучения основ компьютерной грамотности и практических занятий обучающихся.",
+    images: [
+      'fs-objects/cab-technology.jpg',
+      'fs-objects/cab-informatics.jpg',
+      'fs-objects/cab-technology.jpg',
+      'fs-objects/cab-informatics.jpg',
+    ],
+  },
+  {
+    title: "Кабинет технологии",
+    text: "<p>Объект предназначен для проведения практических работ формирующих представления о составляющих техносферы, о современном производстве и о распространенных в нем технологиях.Кабинет оборудован швейными машинами, доской гладильной. Место учителя оборудовано (проектор, экран).</p><p><span>Мастерская</span> — основная учебно-методическая и практическая база для выполнения учебных программ по предмету «Технология» для учащихся 5-8 классов.</p>",
+    images: [
+      'fs-objects/cab-technology.jpg',
+      'fs-objects/cab-informatics.jpg',
+      'fs-objects/cab-technology.jpg',
+      'fs-objects/cab-informatics.jpg',
+      'fs-objects/cab-technology.jpg',
+    ],
+  },
+  {
+    title: "Библиотека",
+    text: "Библиотека предназначена для самостоятельной работы с учебно-методической литературой и цифровыми образовательными ресурсами.",
+    images: [
+      'fs-objects/cab-technology.jpg',
+      'fs-objects/cab-informatics.jpg',
+      'fs-objects/cab-technology.jpg',
+      'fs-objects/cab-informatics.jpg',
+      'fs-objects/cab-technology.jpg',
+      'fs-objects/cab-informatics.jpg',
+    ],
+  },
+]
