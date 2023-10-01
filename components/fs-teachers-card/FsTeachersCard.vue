589214f55111fa60406afe14ec8782fbaf974508
DmitriyYar <1120597@mail.ru>
2023-10-01 22:38:59 +0400

    editing a branch employees

--- components/fs-teachers-card/FsTeachersCard.vue
+++ components/fs-teachers-card/FsTeachersCard.vue
@@ -31,38 +31,35 @@
           'fs-teachers__info-title--thr': status === 'teacher',
         }"
       >
-        <div v-if="status === 'admin'">
-          <p v-for="(name, key) in getName(teacher.name)" :key="key">{{ name }}</p>
-        </div>
-        <!-- <span v-for="(name, key) in getName(teacher.name)" :key="key">{{ name }}</span> -->
-        <p v-if="status === 'teacher'">{{ teacher.name }}</p>
+        <p><span v-for="(name, key) in getNameThr(teacher.name)" :key="key">{{ name }}</span></p>
+
       </div>
       <div class="fs-teachers__info-description">
-        <div class="fs-teachers__info-line">
+        <div class="fs-teachers__info-line" v-if="teacher.post">
           <p>Должность:</p>
           <p>{{ teacher.post }}</p>
         </div>
-        <div class="fs-teachers__info-line">
+        <div class="fs-teachers__info-line" v-if="teacher.speciality">
           <p>Специальность:</p>
           <p>{{ teacher.speciality }}</p>
         </div>
-        <div class="fs-teachers__info-line">
+        <div class="fs-teachers__info-line" v-if="teacher.totalExperience">
           <p>Образование:</p>
           <p>{{ teacher.education }}</p>
         </div>
-        <div class="fs-teachers__info-line">
+        <div class="fs-teachers__info-line" v-if="teacher.totalExperience">
           <p>Общий стаж:</p>
           <p>{{ teacher.totalExperience }}</p>
         </div>
-        <div class="fs-teachers__info-line">
+        <div class="fs-teachers__info-line" v-if="teacher.generalTeachingExperience">
           <p>Общий педагогический стаж:</p>
           <p>{{ teacher.generalTeachingExperience }}</p>
         </div>
-        <div class="fs-teachers__info-line">
+        <div class="fs-teachers__info-line" v-if="teacher.tel">
           <p>Телефон:</p>
           <p>{{ teacher.tel }}</p>
         </div>
-        <div class="fs-teachers__info-line">
+        <div class="fs-teachers__info-line" v-if="teacher.email">
           <p>Электронная почта:</p>
           <p>{{ teacher.email }}</p>
         </div>
@@ -81,7 +78,14 @@ export default {
   props: ["teacher", "status"],
   mixins: [imageUrl],
   methods: {
-     getName(name) {
+    getName(name) {
+      let arrName = [];
+      const [lastName, ...firstName] = name.split(" ");
+      arrName.push(lastName, firstName.join(" "));
+      return arrName;
+      // return name.split(" ");
+    },
+    getNameThr(name) {
       let arrName = [];
       const [lastName, ...firstName] = name.split(" ");
       arrName.push(lastName, firstName.join(" "));
@@ -91,7 +95,8 @@ export default {
     errorImg(el) {
       console.log(el);
       if (this.status === "teacher") {
-        el.target.style.visibility = "hidden";
+        // el.target.style.visibility = "hidden";
+        el.target.style.display = "none";
       }
     },
   },
