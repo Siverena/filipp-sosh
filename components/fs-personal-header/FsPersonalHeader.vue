0a9f09ee8d376effdc250591d799f141ae516194
Siverena <vpupkin@company.com>
2023-10-12 19:47:42 +0300

    lk

---
+++ components/fs-personal-header/FsPersonalHeader.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="fs-personal-header__info">
+        <div class="fs-personal-header__col-left">
+            <div class="fs-personal-header__photo">
+                <img
+                    :src="file ? fileUrl : getUser.avatar ? getUser.avatar : ''"
+                    alt=""
+                />
+                <label
+                    for="input"
+                    class="fs-personal-header__add-photo"
+                    :class="{
+                        'fs-personal-header__add-photo--user': getUser.avatar,
+                    }"
+                >
+                    <FsAddPhoto />
+                    <input
+                        type="file"
+                        name="f"
+                        accept="image/*"
+                        id="input"
+                        v-on:change="handleFileUpload()"
+                    />
+                </label>
+            </div>
+            <div
+                v-if="getUserData.isTeacher"
+                class="fs-personal-header__admin-links"
+            >
+                <nuxt-link to="/personal/questions">Вопросы</nuxt-link>
+            </div>
+        </div>
+        <div class="fs-personal-header__col-right">
+            <div class="fs-personal-header__name">
+                {{ getUser.name }}
+            </div>
+
+            <div class="fs-personal-header__user-content">
+                <div class="fs-personal-header__info-block">
+                    <div class="fs-personal-header__field">
+                        <div class="fs-personal-header__field-name">Email</div>
+                        <div class="fs-personal-header__field-value">
+                            {{ getUser.email }}
+                        </div>
+                    </div>
+                    <div class="fs-personal-header__field">
+                        <div class="fs-personal-header__field-name">
+                            Телефон
+                        </div>
+                        <div class="fs-personal-header__field-value">
+                            {{ getUser.phone }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { mapActions, mapState } from 'pinia';
+import { useScheduleStore } from '@/stores/scheduleStore.js';
+import { useUserStore } from '@/stores/userStore.js';
+export default {
+    data() {
+        return {
+            loading: true,
+            file: '',
+            formData: new FormData(),
+        };
+    },
+    computed: {
+        ...mapState(useUserStore, ['getUser', 'getUserData']),
+        fileUrl() {
+            return URL.createObjectURL(this.file);
+        },
+    },
+    watch: {
+        getUserData() {
+            this.loading = false;
+        },
+    },
+    methods: {
+        ...mapActions(useUserStore, ['changePhoto']),
+
+        getImg() {},
+        handleFileUpload() {
+            this.file = input.files[0];
+            const reader = new FileReader();
+            this.submitFile();
+        },
+        async submitFile() {
+            this.formData.append('image', this.file);
+            this.changePhoto(this.formData);
+        },
+    },
+    async created() {},
+};
+</script>
--- pages/personal/index.vue
+++ pages/personal/index.vue
@@ -4,83 +4,11 @@
             <Title>Личный кабинет | МКОУ ФИЛИППОВСКАЯ СОШ</Title>
         </Head>
         <div class="container">
-            <FsSectionTitles>
-                <template v-slot:main>Личный кабинет</template>
-            </FsSectionTitles>
-
-            <div class="fs-personal__info">
-                <div class="fs-personal__col-left">
-                    <div class="fs-personal__photo">
-                        <img src="@/assets/img/fs-person/person.png" alt="" />
-                    </div>
-                    <p>
-                        <input
-                            type="file"
-                            name="f"
-                            accept="image/*"
-                            id="input"
-                            v-on:change="handleFileUpload()"
-                        />
-                        <input type="submit" value="Отправить" />
-                    </p>
-                </div>
-                <div class="fs-personal__col-right">
-                    <div class="fs-personal__name">
-                        {{ getUser.name }}
-                    </div>
-                    <div class="fs-personal__info-block">
-                        <div class="fs-personal__field">
-                            <div class="fs-personal__field-name">Email</div>
-                            <div class="fs-personal__field-value">
-                                {{ getUser.email }}
-                            </div>
-                        </div>
-                        <div class="fs-personal__field">
-                            <div class="fs-personal__field-name">Телефон</div>
-                            <div class="fs-personal__field-value">
-                                {{ getUser.phone }}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <FsCallSchedule class="fs-personal__shedule" />
-                <div class="fs-personal__admin-links">
-                    <nuxt-link to="/personal/questions">Вопросы</nuxt-link>
-                </div>
-            </div>
-            <div class="fs-personal__shedule">
-                <p class="fs-h2 fs-schedule__title">Расписание уроков</p>
-                <div class="fs-schedule__loader" v-if="loading">
-                    <div class="fs-schedule__loader-wrapper">
-                        <div class="fs-schedule__loader-line"></div>
-                        <div class="fs-schedule__loader-line"></div>
-                        <div class="fs-schedule__loader-line"></div>
-                    </div>
-                </div>
-                <div v-else class="fs-schedule__items">
-                    <div
-                        class="fs-schedule-day"
-                        v-for="(day, index) in getSchedule.days"
-                        :key="index"
-                    >
-                        <div class="fs-schedule-day__title">
-                            {{ day.dayName }}
-                        </div>
-                        <ol class="fs-schedule-day__list" v-if="day.lessons[0]">
-                            <template
-                                v-for="(lesson, i) in day.lessons"
-                                :key="i"
-                            >
-                                <li class="fs-schedule-day__name">
-                                    {{ lesson }}
-                                </li>
-                            </template>
-                        </ol>
-                        <div v-else>Выходной</div>
-                    </div>
-                </div>
-            </div>
-
+            <FsPersonalHeader />
+            <FsCallSchedule
+                v-if="!getUserData.isTeacher"
+                class="fs-personal-header__shedule"
+            />
             <Accordion>
                 <AccordionTab>
                     <template v-slot:title> 1Б класс (213каб) </template>
@@ -103,44 +31,28 @@
 </template>
 <script>
 import { mapActions, mapState } from 'pinia';
-import { useScheduleStore } from '@/stores/scheduleStore.js';
 import { useUserStore } from '@/stores/userStore.js';
 export default {
     data() {
         return {
             loading: true,
-            file: '',
-            formData: new FormData(),
         };
     },
     computed: {
-        ...mapState(useScheduleStore, ['getSchedule']),
-        ...mapState(useUserStore, ['getUser']),
+        ...mapState(useUserStore, ['getUser', 'getDataLoading', 'getUserData']),
+    },
+    watch: {
+        getUserData() {
+            this.loading = false;
+        },
     },
     methods: {
-        ...mapActions(useScheduleStore, ['fetchSchedule', 'changePhoto']),
-        async loadData() {
-            try {
-                this.loading = true;
-                await this.fetchSchedule('1');
-
-                this.loading = false;
-                console.log(this.getSchedule);
-            } catch (e) {
-                console.log(e);
-            }
-        },
-        getImg() {},
-        handleFileUpload() {
-            this.file = this.$refs.file.files[0];
-        },
-        submitFile() {
-            this.formData.append('file', this.file);
-            this.changePhoto(this.formData);
-        },
-    },
-    async created() {
-        this.loadData();
+        ...mapActions(useUserStore, [
+            'fetchUserData',
+            'auth',
+            'logout',
+            'checkAuth',
+        ]),
     },
 };
 </script>
