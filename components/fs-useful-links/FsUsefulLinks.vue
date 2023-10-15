b777da0dcf39ce9ca111c8202753646757375b46
vadimedu <vadimedu@yandex.ru>
2023-10-15 11:28:11 +0300

    add responsive styles

--- components/fs-useful-links/FsUsefulLinks.vue
+++ components/fs-useful-links/FsUsefulLinks.vue
@@ -4,10 +4,10 @@
         <div class="fs-useful-links__items" v-for="item in getLinks">
         <span>{{ item.bannerTitle }}</span>
         <nuxt-link v-if="item.slug !== ''" :to="`/banner/${item.slug}`">
-        <img height="110" width="253" src="../../public/images/fs-news/img.jpeg" alt="foto">
+        <img src="../../public/images/fs-news/img.jpeg" alt="foto">
         </nuxt-link>
         <a v-if="item.slug === ''" :href="item.ref">
-            <img height="110" width="253" src="../../public/images/fs-news/img.jpeg" alt="foto">
+            <img src="../../public/images/fs-news/img.jpeg" alt="foto">
         </a>
     </div>
     </div>
